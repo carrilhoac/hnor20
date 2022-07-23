eebd0138ef00399f968257387de15f3bd34d60eb
André Caceres Carrilho <carrilho.acc@gmail.com>
2022-07-22 22:25:13 -0300

    Code rev1

--- hnor20.cpp
+++ hnor20.cpp
@@ -1,30 +1,5 @@
 
-#include <fstream>
-#include <iostream>
-
-class C_hnor {
-private:
-	double **				    _fator;
-
-	static constexpr int 		_ncols      = 540;
-	static constexpr int 		_nrows      = 492;
-
-	static constexpr double 	_step       = 1.0 / 12.0;
-
-	static constexpr double 	_ilon       = 285.0 + (1.0 / 24.0);  // 285.0416667
-	static constexpr double 	_ilat       = 5.95  + (1.0 / 120.0); //   5.9583333
-
-private:
-	void 					    _MemAlloc(void);
-	void 					    _MemFree(void);
-
-	bool					    _ReadTextFile(const char *txt_file);
-	static bool				    _ReadDouble(std::ifstream& in_file, double *val = nullptr);
-
-public:
-	C_hnor(void);
-	~C_hnor(void);
-};
+#include "hnor20.h"
 
 bool C_hnor::_ReadDouble(std::ifstream& in_file, double *val)
 {
@@ -107,11 +82,4 @@ void C_hnor::_MemFree(void)
 		delete [] _fator;
 		_fator = nullptr;
 	}
-}
-
-int main(int argc, char **argv)
-{
-    C_hnor grid;
-
-	return 0;
 }
---
+++ main.cpp
@@ -0,0 +1,9 @@
+
+#include "hnor20.h"
+
+int main(int argc, char **argv)
+{
+    C_hnor grid;
+
+	return 0;
+}
