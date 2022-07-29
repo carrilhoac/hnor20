f24903a2711991db82821922d3bd3ae57e4ea315
André <carrilho.acc@gmail.com>
2022-07-29 07:34:32 -0700

    Argument Parsing

--- main.cpp
+++ main.cpp
@@ -5,19 +5,82 @@
 #include "args.h"
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
 
 class CAppHnor {
+private:
+	struct HnorInput  {
+		double d_lat;
+		double d_lon;
+	};
 private:
 	CArgParse 	args;
 	HNOR 		geoid;
 		
+	std::vector<PointResult>  result;
+	std::vector<HnorInput>  inputs;
+	
 public:
 	CAppHnor(int argc, char **argv);
 	
+	void ParseLine(const std::string& s_line, int col_lat, int col_lon)
+	{		
+		std::stringstream ss(s_line);
+		std::vector<std::string> s_tokens;
+		std::string s_piece;
+		
+		while (ss.good())
+		{
+			ss >> s_piece;
+			s_tokens.push_back( s_piece );
+		}
+		
+		HnorInput c_point;
+		c_point.d_lat = std::stod( s_tokens[col_lat] );
+		c_point.d_lon = std::stod( s_tokens[col_lon] );
+		inputs.push_back( c_point );
+		
+		std::cout << c_point.d_lat << " " << c_point.d_lon << std::endl;
+	}
+	
+	bool ReadTextFile(const std::string& txt_file_name, int col_lat, int col_lon)
+	{
+		std::ifstream in_file;
+		in_file.open(txt_file_name.c_str());
+		
+		while (in_file.good())
+		{
+			std::string s_curline;
+			std::getline(in_file, s_curline);
+						
+			ParseLine(s_curline, col_lat, col_lon);
+		}
+		
+		in_file.close();
+		return true;
+	}
+	
 	void ExecSinglePoint(void)
 	{
-		std::cout << args.GetValueDbl("lat") << std::endl;
-		std::cout << args.GetValueDbl("lon") << std::endl;
+		double lat = args.GetValueDbl("lat");
+		double lon = args.GetValueDbl("lon");
+		
+		result.push_back( geoid.Get(lat, lon) );
+		std::cout << result.back() << std::endl;
+	}
+	
+	void ExecTextFile(void)
+	{
+		int n_col_lat = 0;
+		int n_col_lon = 1;
+				
+		if (args.CheckOption("col_lat"))
+			n_col_lat = args.GetValueInt("col_lat");
+		if (args.CheckOption("col_lon"))
+			n_col_lon = args.GetValueInt("col_lon");
+				
+		ReadTextFile( args.GetValueStr("in"), n_col_lat, n_col_lon );
 	}
 	
 	bool DelegateTask(void)
@@ -27,6 +90,12 @@ public:
 			ExecSinglePoint();
 			return true;
 		}		
+		
+		if (args.CheckOption("in"))
+		{
+			ExecTextFile();
+			return true;
+		}
 		
 		std::cout << Help();
 		return false;
